3c25ccc93e6ac29cfa75d643c0dfb371d3052f64
globalNail <nthnamto147@gmail.com>
2025-12-13 12:06:11 +0700

    ref: undelete.sql for improved readability

--- Script/undelete.sql
+++ Script/undelete.sql
@@ -1,9 +1,9 @@
-SELECT TOP (1000)
-    [ClubIDTienPVK]
-      , [ClubCode]
-      , [IsDeleted]
-FROM [FA25_PRN222_3W_PRN222_01_G5_SCMS].[dbo].[ClubsTienPVK]
-
 UPDATE [FA25_PRN222_3W_PRN222_01_G5_SCMS].[dbo].[ClubsTienPVK]
   SET IsDeleted = 0
   WHERE [IsDeleted] = 1
+
+SELECT TOP (1000)
+  [ClubIDTienPVK]
+      , [ClubCode]
+      , [IsDeleted]
+FROM [FA25_PRN222_3W_PRN222_01_G5_SCMS].[dbo].[ClubsTienPVK]
